9aa0cd5559f24e859e83c56419336cab73844dd0
kartikgohad <kartikgohad725@gmail.com>
2024-03-28 17:11:54 +0530

    Push ml code for model

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("Add all ML related code here")
+
+def model1():
+    print("adding code for model 1")
+
+def model2():
+     print("adding code for model 1")
+
+def model3():
+     print("adding code for model 1")
